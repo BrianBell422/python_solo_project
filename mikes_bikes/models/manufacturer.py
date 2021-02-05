f98c8d671070ade97f65ff0d3419e78e784bf405
BrianBell422 <brianbell.uk@gmail.com>
2021-02-05 16:36:40 +0000

    Added Manufacturer class to models dir

---
+++ mikes_bikes/models/manufacturer.py
@@ -0,0 +1,7 @@
+class Manufacturer:
+
+    def __init__(self, name, location, product_type, id = None )
+        self.name = name
+        self.location = location
+        self.product_type = product_type
+        self.id = id
