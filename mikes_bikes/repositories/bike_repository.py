97de73ceaedb6baf1225ef1a449804451e274eca
BrianBell422 <brianbell.uk@gmail.com>
2021-02-05 18:12:51 +0000

    save and delete_all methods added to bike_repository

--- mikes_bikes/repositories/bike_repository.py
+++ mikes_bikes/repositories/bike_repository.py
@@ -3,3 +3,15 @@ from db.run_sql import run_sql
 from models.bike import Bike
 from models.manufacturer import Manufacturer
 import repositories.manufacturer_repository as manufacturer_repository
+
+def save(bike):
+    sql = "INSERT INTO bikes (manufacturer_id, model, description, buy_cost, sell_price) VALUES (%s, %s, %s, %s, %s) RETURNING *"
+    values = [bike.manufacturer.id, bike.model, bike.description, bike.buy_cost, bike.sell_price]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    bike.id = id
+    return bike
+
+def delete_all():
+    sql = "DELETE FROM bikes"
+    run_sql(sql)
