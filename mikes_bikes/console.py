54749f32c2282d4cd41821547e8b16c6b7be7ecd
BrianBell422 <brianbell.uk@gmail.com>
2021-02-05 18:22:26 +0000

    save and delete_all methods added to manufacturer_repository

--- mikes_bikes/console.py
+++ mikes_bikes/console.py
@@ -5,8 +5,10 @@ from models.manufacturer import Manufacturer
 import repositories.bike_repository as bike_repository
 import repositories.manufacturer_repository as manufacturer_repository
 
+
 bike_repository.delete_all()
 manufacturer_repository.delete_all()
+
 
 manufacturer1 = Manufacturer("Honda", "Japan", "Motocross")
 manufacturer_repository.save(manufacturer1)
@@ -17,6 +19,7 @@ manufacturer_repository.save(manufacturer2)
 manufacturer3 = Manufacturer("Yamaha", "Japan", "Road")
 manufacturer_repository.save(manufacturer3)
 
+
 bike1 = Bike("Honda", "CR125", "Motocross", 2500, 5000)
 bike_repository.save(bike1)
 
@@ -25,10 +28,6 @@ bike_repository.save(bike2)
 
 bike3 = Bike("Yamaha", "R1", "Road", 3500, 7000)
 bike_repository.save(bike3)
-
-
-
-
 
 
 pdb.set_trace()
--- mikes_bikes/repositories/manufacturer_repository.py
+++ mikes_bikes/repositories/manufacturer_repository.py
@@ -2,3 +2,15 @@ from db.run_sql import run_sql
 
 from models.manufacturer import Manufacturer
 from models.bike import Bike
+
+def save(manufacturer):
+    sql = "INSERT INTO manufacturers (name, location, product_type) VALUES (%s, %s, %s) RETURNING *"
+    values = [manufacturer.name, manufacturer.location, manufacturer.product_type]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    manufacturer.id = id
+    return manufacturer
+
+def delete_all():
+    sql = "DELETE FROM manufacturers"
+    run_sql(sql)
