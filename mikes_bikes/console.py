603c3f613dbaf96d28d4be16a11b823d635891f2
BrianBell422 <brianbell.uk@gmail.com>
2021-02-05 16:44:12 +0000

    Added db folder, repositories folder and files and console file

---
+++ mikes_bikes/console.py
@@ -0,0 +1,9 @@
+import pdb
+from models.manufacturer import Manufacturer
+from models.bike import Bike
+
+
+
+
+
+pdb.set_trace()
--- mikes_bikes/controllers/manufacturer_controller.py
+++ mikes_bikes/controllers/manufacturer_controller.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, Blueprint
+from models.manufacturer import Manufacturer
 
 manufacturers_blueprint = Blueprint("/manufacturers", __name__)
 
