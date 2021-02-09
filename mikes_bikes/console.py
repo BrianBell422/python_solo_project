be93fa661eb2f721aa5911b9112ca9d24a7b6c12
BrianBell422 <brianbell.uk@gmail.com>
2021-02-09 18:00:09 +0000

    Added extra manufacturers and bikes to console

--- mikes_bikes/console.py
+++ mikes_bikes/console.py
@@ -9,24 +9,42 @@ bike_repository.delete_all()
 manufacturer_repository.delete_all()
 
 
-manufacturer1 = Manufacturer("Honda", "Japan", "Motocross")
+manufacturer1 = Manufacturer("Honda", "Japan", "Off road")
 manufacturer_repository.save(manufacturer1)
 
-manufacturer2 = Manufacturer("Kawasaki", "Japan", "Motocross")
+manufacturer2 = Manufacturer("Kawasaki", "Japan", "Off road")
 manufacturer_repository.save(manufacturer2)
 
-manufacturer3 = Manufacturer("Yamaha", "Japan", "Road")
+manufacturer3 = Manufacturer("Yamaha", "Japan", "On Road")
 manufacturer_repository.save(manufacturer3)
 
+manufacturer4 = Manufacturer("KTM", "Austria", "Off road")
+manufacturer_repository.save(manufacturer4)
 
-bike1 = Bike(manufacturer1, "CR125", "2 Stroke Motocross", 2500, 5000, 10, 50)
+manufacturer5 = Manufacturer("Ducati", "Japan", "On road")
+manufacturer_repository.save(manufacturer5)
+
+manufacturer6 = Manufacturer("Harley Davidson", "USA", "On Road")
+manufacturer_repository.save(manufacturer6)
+
+
+bike1 = Bike(manufacturer1, "CR125", "Motocross", 2500, 5000, 1)
 bike_repository.save(bike1)
 
-bike2 = Bike(manufacturer2, "KXF250", "4 Stroke Motocross", 3000, 6000, 10, 50)
+bike2 = Bike(manufacturer2, "KX250X", "Enduro", 3000, 5200, 5)
 bike_repository.save(bike2)
 
-bike3 = Bike(manufacturer3, "R1", "4 Stroke Road", 3500, 7000, 10, 50)
+bike3 = Bike(manufacturer3, "R1", "Road Race", 3500, 6000, 2)
 bike_repository.save(bike3)
+
+bike4 = Bike(manufacturer4, "SXF350", "Motocross", 3500, 6800, 0)
+bike_repository.save(bike4)
+
+bike5 = Bike(manufacturer5, "748R", "Road Race", 4500, 8200, 7)
+bike_repository.save(bike5)
+
+bike6 = Bike(manufacturer6, "Sportster", "Cruiser Road", 4000, 8000, 4)
+bike_repository.save(bike6)
 
 
 pdb.set_trace()
